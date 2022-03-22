ac8c82ca2c495262173a1d075641834624e6d4b8
chrischen0405 <chrischen0405@gmail.com>
2022-03-22 17:41:37 +0800

    refactor: 批量引入组件

--- widget/index.js
+++ widget/index.js
@@ -1,7 +1,10 @@
-import testWidget from './test-widget'
-
 const install = function (Vue) {
-    Vue.component('test-widget', testWidget)
+    const requireComponents = require.context('./', false, /\.vue$/)
+    requireComponents.keys().forEach(fileName => {
+        const reqCom = requireComponents(fileName)
+        const reqComName = reqCom.default.name || fileName.replace(/\.\/(.*)\.vue/, '$1')
+        Vue.component(reqComName, reqCom.default || reqCom)
+    })
 }
 
 export default {
